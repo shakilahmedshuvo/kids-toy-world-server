b2cc9f50c61f4b6300e4948d3074b109dbd1a963
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-05-19 20:41:25 +0600

    add add to toy post method

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
-const port = process.env.PORT || 5000;
 const app = express();
+const port = process.env.PORT || 5000;
 
 // use middle ware
 app.use(cors());
@@ -11,7 +12,6 @@ app.use(express.json())
 
 // mongodb
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.frhesy5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +27,16 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const toysCollection = client.db('kidsToyWorld').collection('toys');
+
+        // toy post
+        app.post('/addToy', async (req, res) => {
+            const toyData = req.body;
+            const result = await toysCollection.insertOne(toyData);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
